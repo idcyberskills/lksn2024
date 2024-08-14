2a1b459c89dfb068b8c8ca9973434246c03f1f8a
Prajna Prasetya <prajnapras19@gmail.com>
2024-08-14 21:05:13 +0700

    random

--- warmup/randomizer/main.py
+++ warmup/randomizer/main.py
@@ -1,4 +1,9 @@
-from Crypto.Util.number import getPrime, bytes_to_long
+from Crypto.Util.number import bytes_to_long
+import random
+import time
+
+# This app is just for generating random number
+# I don't care what's the number anyway :P
 
 try:
     flag = open('flag/flag.txt', 'rb').read()
@@ -6,8 +11,9 @@ except:
     print('flag missing')
     exit(1)
 
-x = int(input('> ').strip())
-y = x ^ bytes_to_long(flag)
+random.seed(int(time.time()))
+x = random.getrandbits(8 * len(flag))
+y =  x ^ bytes_to_long(flag)
 
-# should just have return a random number :v
+
 print(y)
