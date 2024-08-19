01c4f5486115b1ceadc8977da12def9cdb266ee0
Prajna Prasetya <prajnapras19@gmail.com>
2024-08-19 22:30:33 +0700

    fix start service

--- attack-defense/auther/start.sh
+++ attack-defense/auther/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pip install fastapi uvicorn pydantic
-cd /home/auther
+cd /home/auther/service/
+python3 -m venv env
+source env/bin/activate
+pip install fastapi uvicorn pydantic jwt python-multipart
 uvicorn main:app --reload
