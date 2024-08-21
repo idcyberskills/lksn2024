962c5e900f440167eb3bead5baffb906b16872ad
Prajna Prasetya <prajnapras19@gmail.com>
2024-08-21 16:32:54 +0700

    fix auther

--- attack-defense/auther/start.sh
+++ attack-defense/auther/start.sh
@@ -4,4 +4,4 @@ cd /home/auther/service/
 python3 -m venv env
 source env/bin/activate
 pip install fastapi uvicorn pydantic pyjwt python-multipart
-runcon user_u:user_r:user_t:s0 uvicorn --host 8000 main:app --reload
+uvicorn --host 0.0.0.0 --port 8000 main:app --reload
